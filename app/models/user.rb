3d25f8b360dc0ebed00e37e0a1a725332fa56d91
hugo <a01191470@itesm.mx>
2015-11-18 18:12:39 -0600

    User Model with functionality and validation added.

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+	attr_accessor {}
+	before_save { self.email = email.downcase }
+	
+	validates :address, :telephone, presence: true
+	validates_format_of :email, with: /@/ 
+	validates :telephone, length: {maximum: 11}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: {case_sensitive: false}
+
+    validates :password, presence: true, length: { minimum: 6 }
+
+   has_secure_password 
+end
